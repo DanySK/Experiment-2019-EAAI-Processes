9adef9f55bd5a03c247a15527e911050a1072c5f
Roberto Casadei <robyonrails@gmail.com>
2020-01-10 23:31:02 +0100

    Towards baseline

--- src/main/scala/it/unibo/casestudy/ServiceDiscovery.scala
+++ src/main/scala/it/unibo/casestudy/ServiceDiscovery.scala
@@ -42,32 +42,53 @@ class ServiceDiscovery extends AggregateProgram with StandardSensors with Gradie
   def classicServiceDiscovery() = {
     val hasTask = !task.isEmpty
     val g = classicGradient(hasTask)
+    node.put("gradient", g)
 
-    val localTaskRequest = task.map(t => TaskRequest(mid, t)(allocation = Map.empty))
+    val taskRequest = rep[Option[TaskRequest]](None) { tr =>
+        val localTaskRequest = tr.orElse(task.map(t => TaskRequest(mid, t)(allocation = Map.empty)))
+        val receivedRequest = bcast(hasTask, localTaskRequest)
+        node.put("receivedRequest", receivedRequest.isDefined)
+        node.put("request", receivedRequest)
 
-    val receivedRequest = bcast(hasTask, localTaskRequest)
-
-    val offeredService: Option[Service] = receivedRequest.flatMap { request =>
-      request.allocation.find(_._2==mid).map(_._1) // keep current allocation
-        .orElse[Service] {  // or offer an available service
-          request.missingServices.collectFirst { case s if availableServices.find(_.service==s).isDefined => s }
+        val offeredService: Option[Service] = receivedRequest.flatMap { request =>
+          request.allocation.find(_._2 == mid).map(_._1) // keep current allocation
+            .orElse[Service] { // or offer an available service
+              request.missingServices.collectFirst { case s if availableServices.find(_.service == s).isDefined => s }
+            }
         }
+        node.put("offersService", offeredService.isDefined)
+        node.put("offeredService", offeredService)
+
+        allocatedServices = Set.empty
+        offeredService.foreach { s =>
+          allocatedServices += ProvidedService(s)()
+        }
+
+        val offers = C[Double, Map[ID, Service]](g, _ ++ _, offeredService.map(s => Map(mid -> s)).getOrElse(Map.empty), Map.empty)
+        localTaskRequest.map(t => t.copy()(allocation = chooseFromOffers(t, offers)))
     }
 
-    allocatedServices = Set.empty
-    offeredService.foreach { s =>
-      allocatedServices += ProvidedService(s)()
+    val keepFor: Long = branch(taskRequest.isDefined && taskRequest.get.missingServices.isEmpty){
+      val start = rep(timestamp())(x => x)
+      timestamp() - start
+    }{ 0 }
+    if(keepFor > 10 && node.has("task")){
+      node.remove("task")
     }
-
-    val offers = C[Double, Map[ID,Service]](g, _++_, offeredService.map(s => Map(mid->s)).getOrElse(Map.empty), Map.empty)
-    localTaskRequest.map(t => t.copy()(allocation = t.allocation ++ chooseFromOffers(t, offers)) )
-
   }
 
   def chooseFromOffers(req: TaskRequest, offers: Map[ID,Service]): Map[Service,ID] = {
-    // TODO
-    // offers.filter()
-    Map.empty
+    var servicesToAlloc = req.missingServices
+    var newAllocations = Map[Service,ID]()
+    for(offer <- offers){
+      if(servicesToAlloc.contains(offer._2)){
+        newAllocations += offer.swap
+        servicesToAlloc -= offer._2
+      }
+    }
+
+    // keep current allocations if still provided, and add new allocations
+    req.allocation.filter(curr => offers.contains(curr._2)) ++ newAllocations
   }
 
   def processBasedServiceDiscovery() = {

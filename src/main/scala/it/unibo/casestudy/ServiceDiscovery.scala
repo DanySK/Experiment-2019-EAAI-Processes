b7e1e8e79d89014d03eb5a7d9c559bbb786f407a
Danilo Pianini <danilo.pianini@gmail.com>
2020-01-11 12:25:42 +0100

    Simplify code

--- src/main/scala/it/unibo/casestudy/ServiceDiscovery.scala
+++ src/main/scala/it/unibo/casestudy/ServiceDiscovery.scala
@@ -77,7 +77,7 @@ class ServiceDiscovery extends AggregateProgram with StandardSensors with Gradie
     }
 
     // Time a device is trying to satisfy a task
-    val tryFor: Long = branch(!taskChanged && taskRequest.exists(!_.missingServices.isEmpty) ){
+    val tryFor: Long = branch(!taskChanged && taskRequest.exists(_.missingServices.nonEmpty) ){
       val start = rep(timestamp())(x => x)
       timestamp() - start
     }{ 0 }
